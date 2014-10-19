cfb36fc21df1903dac3154fe75e5766daf11d938
tomasro27 <tomasrodriguez@utexas.edu>
2014-10-18 19:59:14 -0500

    initial commit

---
+++ VRFitness/mobile/src/main/java/com/hacktx/vrfitness/LoginFragment.java
@@ -0,0 +1,28 @@
+package com.hacktx.vrfitness;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by TR on 10/18/2014.
+ */
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle ksavedInstanceState) { super.onCreate(ksavedInstanceState);}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle SavedInstanceState) {
+        View v = inflater.inflate(R.layout.sensor_fragment, parent, false);
+
+
+
+
+        return v;
+    }
+
+}
